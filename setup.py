8335127adbccacf6300d2b658b506bd6051fec38
WoLpH <Rick@Fawo.nl>
2011-11-18 18:54:05 +0100

    disabled inline translations for now

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys, os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.4.15'
+version = '0.4.16'
 
 long_description = (
     read('README.rst')
