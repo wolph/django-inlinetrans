dd69b47cac850b028f5481d9c3d347716e4ad625
WoLpH <Rick@Fawo.nl>
2011-11-18 16:57:37 +0100

    bumped inlinetrans version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys, os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.4.14'
+version = '0.4.15'
 
 long_description = (
     read('README.rst')
